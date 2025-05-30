b6e962a54d6336d3f60440440ffbd13bb33ef85d
Md Shafayatul Haque <shafayat@Mds-MacBook-Air-2.local>
2025-05-31 01:35:56 +0600

    cerp needs more cleaning

--- main_runner.py
+++ main_runner.py
@@ -1,16 +1,28 @@
 import argparse
+import warnings
 
+# Suppress the specific warning from torchvision/io/image.py
+warnings.filterwarnings(
+    "ignore",
+    category=UserWarning,
+    module="torchvision.io.image"
+)
 
-parser = argparse.ArgumentParser(description='PPDL')
-parser.add_argument('--attack', dest='attack', required=True,
-                    choices=['dba', 'cas', 'baseline'],
-                    help='Type of attack to run')
-parser.add_argument('--defense', dest='defense', required=True, choices=['mean', 'pca-deflect'], help='Type of defense to run')
-parser.add_argument('--dataset', dest='dataset', choices=['cifar','mnist','fmnist','emnist'])
-args = parser.parse_args()
-if args.attack == 'dba':
-    import attacks.DBA.main as main
-    main.main_dba(args.defense, args.dataset)
-elif args.attack == 'cas':
-    import attacks.ConstrainScale.main as main
-    main.main_cas(args.defense, args.dataset)
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='PPDL')
+    parser.add_argument('--attack', dest='attack', required=True,
+                        choices=['dba', 'cas', 'cerp'],
+                        help='Type of attack to run')
+    parser.add_argument('--defense', dest='defense', required=True, choices=['mean', 'pca-deflect'], help='Type of defense to run')
+    parser.add_argument('--dataset', dest='dataset', choices=['cifar','mnist','fmnist','emnist'])
+    args = parser.parse_args()
+    if args.attack == 'dba':
+        import attacks.DBA.main as main
+        main.main_dba(args.defense, args.dataset)
+    elif args.attack == 'cas':
+        import attacks.ConstrainScale.main as main
+        main.main_cas(args.defense, args.dataset)
+    elif args.attack == 'cerp':
+        import attacks.CerP.main as main
+        main.main_cerp(args.defense, args.dataset)
